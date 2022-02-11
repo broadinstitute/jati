bc450e40cccd3bd0a31287ae308ff1e1eb05800d
curoli <curoli@gmail.com>
2022-02-11 17:13:52 -0500

    Token knows start and end pos.

--- src/token.rs
+++ src/token.rs
@@ -5,12 +5,13 @@ pub(crate) mod token_iter;
 
 pub struct Token<T> {
     item: Box<T>,
-    pos: Pos,
+    pos_begin: Pos,
+    pos_end: Pos
 }
 
 impl<T: 'static> Token<T> {
-    pub(crate) fn new(item: T, pos: Pos) -> Token<T> {
+    pub(crate) fn new(item: T, pos_begin: Pos, pos_end: Pos) -> Token<T> {
         let item = Box::new(item);
-        Token { item, pos }
+        Token { item, pos_begin, pos_end }
     }
 }
--- src/token/code_point.rs
+++ src/token/code_point.rs
@@ -4,10 +4,13 @@ use crate::token::token_iter::TokenResult;
 use crate::error::ParseError;
 use crate::line_break::LineBreaker;
 use crate::token::Token;
+use core::mem;
 
 pub(crate) struct CodePointIter {
     bytes_iter: Box<dyn Iterator<Item=u8>>,
     line_breaker: Box<dyn LineBreaker>,
+    pos_begin: Pos,
+    pos_end: Pos,
     pos: Pos,
     failure_opt: Option<ParseError>,
 }
@@ -15,9 +18,11 @@ pub(crate) struct CodePointIter {
 impl CodePointIter {
     pub(crate) fn new(bytes_iter: Box<dyn Iterator<Item=u8>>, line_breaker: Box<dyn LineBreaker>)
                       -> CodePointIter {
+        let pos_begin = Pos::new();
+        let pos_end = Pos::new();
         let pos = Pos::new();
         let failure_opt = None;
-        CodePointIter { bytes_iter, line_breaker, pos, failure_opt }
+        CodePointIter { bytes_iter, line_breaker, pos_begin, pos_end, pos, failure_opt }
     }
 }
 
@@ -31,14 +36,16 @@ impl Iterator for CodePointIter {
             match CodePoint::read(&mut self.bytes_iter) {
                 None => { None }
                 Some(Ok(code_point)) => {
-                    let pos_raw = self.pos.add_char(code_point.n_utf8_bytes() as usize);
-                    let pos =
+                    let pos_unbroken = self.pos.add_char(code_point.n_utf8_bytes() as usize);
+                    let pos_end_new =
                         if self.line_breaker.check_if_new_line(&code_point) {
-                            pos_raw.break_line()
+                            pos_unbroken.break_line()
                         } else {
-                            pos_raw
+                            pos_unbroken
                         };
-                    Some(Ok(Token::new(code_point, pos)))
+                    self.pos_begin = mem::replace(&mut self.pos_end, pos_end_new);
+                    Some(Ok(Token::new(code_point, self.pos_begin.clone(),
+                                       self.pos_end.clone())))
                 }
                 Some(Err(utf8_error)) => {
                     let pos = self.pos.add_char(utf8_error.i_byte as usize);
