ae60542f8df99b590179f1c120a41dec2b9b21bc
curoli <curoli@gmail.com>
2022-04-19 21:45:20 -0400

    Expand and fix Rule API.

--- src/rule.rs
+++ src/rule.rs
@@ -1,16 +1,36 @@
 use crate::sub_queue::Key;
 use crate::token::Token;
 
-pub(crate) trait Rule<I> {
-    fn apply(&self, key: Key, item: &I) -> Vec<MatchPath<I>>;
+#[derive(PartialOrd, Ord, PartialEq, Eq)]
+pub(crate) struct Precedence {
+    precedence: u8
 }
 
-pub(crate) enum MatchPath<I> {
+impl From<u8> for Precedence {
+    fn from(precedence: u8) -> Self { Precedence { precedence } }
+}
+
+pub(crate) enum Associativity {
+    LeftToRight,
+    RightToLeft
+}
+
+pub(crate) trait Rule<I> {
+    fn precedence(&self) -> Precedence;
+    fn associativity(&self) -> Associativity;
+    fn apply(&self, key: Key, item: &I) -> Vec<Match<I>>;
+}
+
+pub struct Match<I> {
+    path: Vec<Key>,
+    application: Application<I>
+}
+
+pub(crate) enum Application<I> {
     Incomplete(Box<dyn Rule<I>>),
     Complete(Substitution<I>)
 }
 
 pub struct Substitution<I> {
-    deletion: Vec<Key>,
     insertion: Vec<Token<I>>
 }
