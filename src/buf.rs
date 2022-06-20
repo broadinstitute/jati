17d85d2adf9ec59c8d715772df6bf299761f0785
curoli <curoli@gmail.com>
2022-06-20 14:23:31 -0400

    Buf, Cursor, part 2.

--- src/buf.rs
+++ src/buf.rs
@@ -1,16 +1,35 @@
 use crate::error::Error;
 
 struct Buf<T: Copy> {
-    items: Vec<Vec<T>>
+    items: Vec<Vec<T>>,
 }
 
 impl<T: Copy> Buf<T> {
-    fn get_cursor_at(&self, i: usize) -> Result<Cursor, Error> {
-        todo!()
+    fn get_cursor_at(&self, i: usize) -> Result<Cursor<T>, Error> {
+        let mut i_chunk: usize = 0;
+        let mut i_item: usize = i;
+        loop {
+            match self.items.get(i_chunk) {
+                Some(chunk) => {
+                    if i_item < chunk.len() {
+                        Ok(Cursor::new(self, i_chunk, i_item))
+                    }
+                    todo!()
+                }
+                None => { todo!() }
+            }
+        }
     }
 }
 
-struct Cursor {
+struct Cursor<'a, T: Copy> {
+    buf: &'a Buf<T>,
     i_chunk: usize,
-    i_item: usize
+    i_item: usize,
+}
+
+impl Cursor<T> {
+    pub(crate) fn new(buf: &Buf<T>, i_chunk: usize, i_item: usize) -> Cursor<T> {
+        Cursor { buf, i_chunk, i_item}
+    }
 }
