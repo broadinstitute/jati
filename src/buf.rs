a79ecf00f52a4bb74f23538d454eba3ada882f4d
curoli <curoli@gmail.com>
2022-06-27 17:58:38 -0400

    Reimplement buffer using BufBox.

--- src/buf.rs
+++ src/buf.rs
@@ -1,56 +1,45 @@
-use crate::error::{Error, ErrorKind};
+use std::ops::Deref;
+use std::sync::Arc;
 
-pub(crate) struct Buf<T: Copy> {
-    items: Vec<Vec<T>>,
-    len_sums: Vec<usize>,
-}
-
-fn new_len_sums<T>(vecs: &[Vec<T>]) -> Vec<usize> {
-    let mut sum: usize = 0;
-    let mut size_sums: Vec<usize> = Vec::new();
-    for vec in vecs {
-        sum += vec.len();
-        size_sums.push(sum);
-    }
-    size_sums
-}
-
-impl<T: Copy> Buf<T> {
-    pub(crate) fn new(items: Vec<Vec<T>>) -> Buf<T> {
-        let size_sums = new_len_sums(&items);
-        Buf { items, len_sums: size_sums }
-    }
-    pub fn len(&self) -> usize { *self.len_sums.last().unwrap_or(&0usize) }
-    fn get_cursor_at(&self, i: usize) -> Result<Cursor<T>, Error> {
-        let mut i_chunk: usize = 0;
-        let mut i_item: usize = i;
-        loop {
-            match self.items.get(i_chunk) {
-                Some(chunk) => {
-                    if i_item < chunk.len() {
-                        break Ok(Cursor::new(self, i_chunk, i_item));
-                    } else {
-                        i_chunk += 1;
-                        i_item -= chunk.len();
-                    }
-                }
-                None => {
-                    break Err(Error::new(ErrorKind::OutOfRange,
-                                         format!("{} is out of range", i)));
-                }
-            }
-        }
-    }
-}
-
-struct Cursor<'a, T: Copy> {
-    buf: &'a Buf<T>,
+struct Pos {
     i_chunk: usize,
-    i_item: usize,
+    i_in_chunk: usize
 }
 
-impl<'a, T: Copy> Cursor<'a, T> {
-    pub(crate) fn new(buf: &Buf<T>, i_chunk: usize, i_item: usize) -> Cursor<T> {
-        Cursor { buf, i_chunk, i_item }
+pub struct Buf {
+    chunks: Vec<Vec<u8>>
+}
+
+pub(crate) struct BufBox {
+    buf: Arc<Buf>
+}
+
+impl Buf {
+    pub(crate) fn new(chunks: Vec<Vec<u8>>) -> Buf {
+        Buf { chunks }
+    }
+    pub(crate) fn len(&self) -> usize {
+        todo!()
+    }
+}
+
+impl BufBox {
+    pub(crate) fn new(buf: Arc<Buf>) -> BufBox {
+        BufBox { buf }
+    }
+}
+
+impl Clone for BufBox {
+    fn clone(&self) -> Self {
+        let buf = self.buf.clone();
+        BufBox { buf }
+    }
+}
+
+impl Deref for BufBox {
+    type Target = Buf;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.buf
     }
 }
--- src/jati.rs
+++ src/jati.rs
@@ -1,13 +1,15 @@
-use crate::buf::Buf;
+use std::sync::Arc;
+use crate::buf::{Buf, BufBox};
 
 pub struct Jati {
-    buf: Buf<u8>,
+    buf: BufBox,
 }
 
 impl Jati {
     pub fn new(data: Vec<u8>) -> Jati {
-        let buf = Buf::new(vec!(data));
+        let buf = BufBox::new(Arc::new(Buf::new(vec!(data))));
         Jati { buf }
     }
     pub fn len_buffer(&self) -> usize { self.buf.len() }
 }
+
