d3a09b692cd781a3c7c58020a9c98156a7b424c2
curoli <curoli@gmail.com>
2022-06-27 18:07:03 -0400

    Implementing Buf.len().

--- src/buf.rs
+++ src/buf.rs
@@ -3,23 +3,34 @@ use std::sync::Arc;
 
 struct Pos {
     i_chunk: usize,
-    i_in_chunk: usize
+    i_in_chunk: usize,
 }
 
 pub struct Buf {
-    chunks: Vec<Vec<u8>>
+    chunks: Vec<Vec<u8>>,
+    len_sums: Vec<usize>,
 }
 
 pub(crate) struct BufBox {
-    buf: Arc<Buf>
+    buf: Arc<Buf>,
 }
 
 impl Buf {
+    fn get_len_sums(chunks: &Vec<Vec<u8>>) -> Vec<usize> {
+        let mut len_sums: Vec<usize> = Vec::new();
+        let mut len_sum: usize = 0;
+        for chunk in chunks {
+            len_sum += chunk.len();
+            len_sums.push(len_sum);
+        }
+        len_sums
+    }
     pub(crate) fn new(chunks: Vec<Vec<u8>>) -> Buf {
-        Buf { chunks }
+        let len_sums = Buf::get_len_sums(&chunks);
+        Buf { chunks, len_sums }
     }
     pub(crate) fn len(&self) -> usize {
-        todo!()
+        *self.len_sums.last().unwrap_or(&0usize)
     }
 }
 
