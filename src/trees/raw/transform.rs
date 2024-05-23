50444d39ec115fa8337b36063f07e42b9b0a6abd
curoli <oliverr@broadinstitute.org>
2024-05-23 16:53:16 -0400

    Cleanup.

--- src/trees/raw/transform.rs
+++
@@ -1,7 +0,0 @@
-use crate::trees::MaybeChanged;
-use crate::trees::raw::tree::Tree;
-use crate::util::meta::ValueMeta;
-
-pub trait Transformer {
-    fn transform(&self, tree: Tree) -> ValueMeta<Tree, MaybeChanged>;
-}
