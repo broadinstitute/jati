f532582736fb4037ff97c5edcabd7a7e20d5b027
curoli <oliverr@broadinstitute.org>
2024-05-31 17:40:32 -0400

    More on execution.

--- src/trees/execute.rs
+++ src/trees/execute.rs
@@ -1,3 +1,4 @@
+use std::marker::PhantomData;
 use crate::run::RunState;
 use crate::trees::typed::tree::Tree;
 use crate::trees::values::Value;
@@ -6,9 +7,26 @@ pub trait Executor<R: RunState, E: std::error::Error> {
     fn execute(&self, tree: Tree, state: &mut R) -> Result<Value, E>;
 }
 
-pub struct SimpleExecutor<R: RunState, E: std::error::Error> {}
-
-impl<R: RunState, E: std::error::Error> SimpleExecutor<R, E> {
-    pub fn new() -> Self { SimpleExecutor {} }
+pub struct SimpleExecutor<R: RunState, E: std::error::Error> {
+    run_state_phantom: PhantomData<R>,
+    error_phantom: PhantomData<E>,
 }
 
+impl<R: RunState, E: std::error::Error> SimpleExecutor<R, E> {
+    pub fn new() -> Self { SimpleExecutor { run_state_phantom: Default::default(), error_phantom: Default::default() } }
+}
+
+
+impl<R: RunState, E: std::error::Error> Executor<R, E> for SimpleExecutor<R, E> {
+    fn execute(&self, tree: Tree, state: &mut R) -> Result<Value, E> {
+        match tree {
+            Tree::Var(_) => { todo!() }
+            Tree::Literal(_) => { todo!() }
+            Tree::Operation(_) => { todo!() }
+        }
+    }
+}
+
+impl<R: RunState, E: std::error::Error> Default for SimpleExecutor<R, E> {
+    fn default() -> Self { SimpleExecutor::new() }
+}
