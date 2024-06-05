0499bbab24c5cd6a5e01d26c2d68a3714230f9f2
curoli <oliverr@broadinstitute.org>
2024-06-05 15:42:58 -0400

    Fix execute arguments.

--- src/trees/execute.rs
+++ src/trees/execute.rs
@@ -5,7 +5,7 @@ use crate::trees::typed::tree::Tree;
 use crate::trees::values::Value;
 
 pub trait Executor<R: Runtime> {
-    fn execute(&self, tree: Tree, runtime: &mut R, symbols: &mut R::S) -> Result<Value, R::E>;
+    fn execute(&self, tree: &Tree, runtime: &mut R, symbols: &mut R::S) -> Result<Value, R::E>;
 }
 
 pub struct SimpleExecutor<R: Runtime> {
@@ -20,17 +20,22 @@ impl<R: Runtime> SimpleExecutor<R> {
 
 
 impl<R: Runtime> Executor<R> for SimpleExecutor<R> {
-    fn execute(&self, tree: Tree, runtime: &mut R, symbols: &mut R::S) -> Result<Value, R::E> {
+    fn execute(&self, tree: &Tree, runtime: &mut R, symbols: &mut R::S) -> Result<Value, R::E> {
         match tree {
             Tree::Var(var) => { runtime.get_var_value(&var.tag.key) }
-            Tree::Literal(value) => { Ok(value) }
+            Tree::Literal(value) => { Ok(value.clone()) }
             Tree::Op(op) => {
                 let mut values = Vec::with_capacity(op.kids.len());
-                for kid in op.kids {
+                for kid in &op.kids {
                     values.push(self.execute(kid, runtime, symbols)?);
+                    if runtime.stop_requested() { break; }
                 }
-                let op_fn = runtime.get_op_func(op.op.key())?;
-                (op_fn.func)(&values, runtime, symbols)
+                if runtime.stop_requested() {
+                    Ok(Value::Never)
+                } else {
+                    let op_fn = runtime.get_op_func(op.op.key())?;
+                    (op_fn.func)(&values, runtime, symbols)
+                }
             }
         }
     }
