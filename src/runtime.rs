165613a9e5a8c5cc3a67dd03ba375537446f5bc5
curoli <oliverr@broadinstitute.org>
2024-06-12 09:48:30 -0400

    Fix: remove runtime functions that cover things already handled by the symbol table.

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,15 +1,8 @@
-use crate::symbols::ops::OpKey;
 use crate::symbols::symbol_table::SymbolTable;
-use crate::symbols::var::VarKey;
-use crate::trees::values::Value;
 
 pub trait Runtime {
     type S: SymbolTable<Self>;
     type E: std::error::Error;
     fn request_stop(&mut self);
     fn stop_requested(&self) -> bool;
-    fn set_var_value(&mut self, key: &VarKey, value: Value) -> Result<(), Self::E>;
-    fn get_var_value(&self, key: &VarKey) -> Result<Value, Self::E>;
-    fn apply_func(&mut self, key: &OpKey, args: &[Value], symbols: &mut Self::S)
-                 -> Result<Value, Self::E>;
 }
