626bd6d5aa26cc63a61a97afb65a228678ea86c4
curoli <oliverr@broadinstitute.org>
2024-11-05 17:27:08 -0500

    New parsers, part 1.

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,20 @@
+use crate::error::Error;
+
 pub(crate) mod error;
 pub mod parsers;
 
+pub trait Parser {
+    type Output;
+    fn parse<B: Iterator<Item=u8> + Clone>(bytes: B) -> ParseResult<Self::Output>;
+}
+
+pub enum ParseResult<O> {
+    Success(O),
+    Error(Error),
+    Failure(Failure),
+}
+
+pub struct Failure {
+    actual: String,
+    expected: String,
+}
