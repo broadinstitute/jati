8755fffb08b57a5b69f4797909ee1003e09d4554
curoli <oliverr@broadinstitute.org>
2025-03-20 15:29:54 -0400

    Add more character classes

--- src/char_pattern.rs
+++ src/char_pattern.rs
@@ -12,6 +12,23 @@ pub enum CharPattern {
 pub enum CharClass {
     Alphabetic,
     Alphanumeric,
+    Ascii,
+    AsciiAlphabetic,
+    AsciiAlphanumeric,
+    AsciiControl,
+    AsciiDigit,
+    AsciiGraphic,
+    AsciiHexDigit,
+    AsciiLowercase,
+    AsciiPunctuation,
+    AsciiUppercase,
+    AsciiWhitespace,
+    Control,
+    Digit { radix: u32 },
+    Lowercase,
+    Numeric,
+    Uppercase,
+    Whitespace,
 }
 
 impl CharClass {
@@ -19,6 +36,23 @@ impl CharClass {
         match self {
             CharClass::Alphabetic => c.is_alphabetic(),
             CharClass::Alphanumeric => c.is_alphanumeric(),
+            CharClass::Ascii => c.is_ascii(),
+            CharClass::AsciiAlphabetic => c.is_ascii_alphabetic(),
+            CharClass::AsciiAlphanumeric => c.is_ascii_alphanumeric(),
+            CharClass::AsciiControl => c.is_ascii_control(),
+            CharClass::AsciiDigit => c.is_ascii_digit(),
+            CharClass::AsciiGraphic => c.is_ascii_graphic(),
+            CharClass::AsciiHexDigit => c.is_ascii_hexdigit(),
+            CharClass::AsciiLowercase => c.is_ascii_lowercase(),
+            CharClass::AsciiPunctuation => c.is_ascii_punctuation(),
+            CharClass::AsciiUppercase => c.is_ascii_uppercase(),
+            CharClass::AsciiWhitespace => c.is_ascii_whitespace(),
+            CharClass::Control => c.is_control(),
+            CharClass::Digit { radix } => c.is_digit(*radix),
+            CharClass::Lowercase => c.is_lowercase(),
+            CharClass::Numeric => c.is_numeric(),
+            CharClass::Uppercase => c.is_uppercase(),
+            CharClass::Whitespace => c.is_whitespace(),
         }
     }
 }
@@ -27,6 +61,23 @@ impl Display for CharClass {
         match self {
             CharClass::Alphabetic => write!(f, "alphabetic"),
             CharClass::Alphanumeric => write!(f, "alphanumeric"),
+            CharClass::Ascii => write!(f, "ASCII"),
+            CharClass::AsciiAlphabetic => write!(f, "ASCII alphabetic"),
+            CharClass::AsciiAlphanumeric => write!(f, "ASCII alphanumeric"),
+            CharClass::AsciiControl => write!(f, "ASCII control"),
+            CharClass::AsciiDigit => write!(f, "ASCII digit"),
+            CharClass::AsciiGraphic => write!(f, "ASCII graphic"),
+            CharClass::AsciiHexDigit => write!(f, "ASCII hex digit"),
+            CharClass::AsciiLowercase => write!(f, "ASCII lowercase"),
+            CharClass::AsciiPunctuation => write!(f, "ASCII punctuation"),
+            CharClass::AsciiUppercase => write!(f, "ASCII uppercase"),
+            CharClass::AsciiWhitespace => write!(f, "ASCII whitespace"),
+            CharClass::Control => write!(f, "control"),
+            CharClass::Digit { radix } => write!(f, "digit (base {})", radix),
+            CharClass::Lowercase => write!(f, "lowercase"),
+            CharClass::Numeric => write!(f, "numeric"),
+            CharClass::Uppercase => write!(f, "uppercase"),
+            CharClass::Whitespace => write!(f, "whitespace"),
         }
     }
 }
@@ -37,13 +88,16 @@ impl CharPattern {
             (CharPattern::Char(match_c), Some(c)) => c == *match_c,
             (CharPattern::Class(class), Some(c)) => class.includes(c),
             (CharPattern::End, None) => true,
-            (CharPattern::Union(matches), _) =>
-                matches.iter().any(|m| m.includes(c)),
+            (CharPattern::Union(matches), _) => matches.iter().any(|m| m.includes(c)),
             _ => false,
         }
     }
-    pub fn for_char(c: char) -> CharPattern { CharPattern::Char(c) }
-    pub fn for_class(class: CharClass) -> CharPattern { CharPattern::Class(class) }
+    pub fn for_char(c: char) -> CharPattern {
+        CharPattern::Char(c)
+    }
+    pub fn for_class(class: CharClass) -> CharPattern {
+        CharPattern::Class(class)
+    }
     pub fn union(self, rhs: CharPattern) -> CharPattern {
         match (self, rhs) {
             (CharPattern::Union(items1), CharPattern::Union(items2)) => {
@@ -71,24 +125,30 @@ impl Display for CharPattern {
                 fmt_char(*c, f)?;
                 write!(f, "'")
             }
-            CharPattern::Class(class) => { write!(f, "{}", class) }
-            CharPattern::Union(matches) => {
-                match matches.len() {
-                    0 => { write!(f, "none") }
-                    1 => { write!(f, "{}", matches[0]) }
-                    _ => {
-                        for (i, char_match) in matches.iter().enumerate() {
-                            if i == matches.len() - 1 {
-                                write!(f, "or {}", char_match)?
-                            } else {
-                                write!(f, "{}, ", char_match)?;
-                            }
-                        }
-                        Ok(())
-                    }
-                }
+            CharPattern::Class(class) => {
+                write!(f, "{}", class)
             }
-            CharPattern::End => { write!(f, "end of input") }
+            CharPattern::Union(matches) => match matches.len() {
+                0 => {
+                    write!(f, "none")
+                }
+                1 => {
+                    write!(f, "{}", matches[0])
+                }
+                _ => {
+                    for (i, char_match) in matches.iter().enumerate() {
+                        if i == matches.len() - 1 {
+                            write!(f, "or {}", char_match)?
+                        } else {
+                            write!(f, "{}, ", char_match)?;
+                        }
+                    }
+                    Ok(())
+                }
+            },
+            CharPattern::End => {
+                write!(f, "end of input")
+            }
         }
     }
 }
