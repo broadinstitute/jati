1d8f73f190020247a83484843ceae5131200533c
curoli <curoli@gmail.com>
2025-04-12 12:52:25 -0400

    Assign, part 2

--- src/parse/parsers/code/assign.rs
+++ src/parse/parsers/code/assign.rs
@@ -1,4 +1,4 @@
-use crate::parse::Parser;
+use crate::parse::{ParseIssue, Parser, Success};
 use crate::trees::tree::Tree;
 use crate::trees::props::Raw;
 pub struct Assign<L, R> where L: Parser<Output=Tree<Raw>>, R: Parser<Output=Tree<Raw>> {
@@ -12,9 +12,9 @@ impl <L, R> Assign<L, R> where L: Parser<Output=Tree<Raw>>, R: Parser<Output=Tre
     }
 }
 
-impl Parser for Assign<L, R> where L: Parser<Output=Tree<Raw>>, R: Parser<Output=Tree<Raw>> {
+impl<L, R> Parser for Assign<L, R> where L: Parser<Output=Tree<Raw>>, R: Parser<Output=Tree<Raw>> {
     type Output = (Tree<Raw>, Tree<Raw>);
-    fn parse<'a>(&self, input: &crate::input::Input<'a>) -> Result<crate::parse::Success<'a, Self::Output>, crate::parse::ParseIssue> {
+    fn parse<'a>(&self, input: &crate::input::Input<'a>) -> Result<Success<'a, Self::Output>, ParseIssue> {
         let Success { remainder: input1, output: output1 } = self.left.parse(input)?;
         let Success { remainder: input2, output: output2 } = self.right.parse(&input1)?;
         Ok(Success { output: (output1, output2), remainder: input2 })
